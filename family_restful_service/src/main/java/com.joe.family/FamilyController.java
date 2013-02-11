ddddf15030a340c6f78080a03cc652c4853688ec
JoeMcKevitt <josephmckevitt@hotmail.com>
2013-02-11 22:59:18 +0000

    2nd commit ever

--- family_restful_service/src/main/java/com.joe.family/FamilyController.java
+++ family_restful_service/src/main/java/com.joe.family/FamilyController.java
@@ -5,15 +5,13 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 /**
  * Created with IntelliJ IDEA.
  * User: pjmckevitt
  * Date: 26/01/2013
  * Time: 23:30
- * To change this template use File | Settings | File Templates.
+ * Controller to server family queries
  */
 
 //http://localhost:9090/family_restful_service/family/list
@@ -25,7 +23,7 @@ public class FamilyController {
     private static List<String> family = new ArrayList<String>();
 
     static   {
-        family.add("Joe");
+        family.add("Joseph");
         family.add("John");
         family.add("James");
         family.add("Mazza");
